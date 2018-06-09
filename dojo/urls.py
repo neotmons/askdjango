ecf1759b17f474e9ce2d3c8a9a7c0186876400cc
neotmons <neotmons@gmail.com>
2018-06-10 00:36:17 +0900

    정규표현식에 대한 실습예제 추가

--- dojo/urls.py
+++ dojo/urls.py
@@ -4,5 +4,7 @@ from django.conf.urls import url
 from dojo import views
 
 urlpatterns = [
-
+    url(r'^sum/(?P<x>\d+)/$', views.mysum),
+    url(r'^sum/(?P<x>\d+)/(?P<y>\d+)/$', views.mysum),
+    url(r'^sum/(?P<x>\d+)/(?P<y>\d+)/(?P<z>\d+)/$', views.mysum),
 ]
