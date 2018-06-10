8bf5c362293a60946513d1a4d9970824d09a17a5
neotmons <neotmons@gmail.com>
2018-06-10 22:45:26 +0900

    blog.admin.py에 admin 추가

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from .models import Post
 
 # Register your models here.
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'created_at', 'updated_at']
+
+#admin.site.register(Post, PostAdmin)
+
--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,26 @@
 # blog/models.py
 
+import re
 from django.db import models
+from django.forms import ValidationError
+
+
+def lnglat_validator(value):
+    if not re.match(r'^([+-]?\d+\.?\d*),([+-]?\d+\.?\d*)$', value):
+        raise ValidationError('Invalid LngLat Type')
+
 
 class Post(models.Model):
+    author = models.CharField(max_length=20, verbose_name='작성자')
     title = models.CharField(max_length=100, verbose_name='제목',
                              help_text='포스팅 제목을 입력해 주세요. 100자 내외')    # 길이 제한이 있는 문자열
     content = models.TextField(verbose_name='내용')                # 길이 제한이 없는 문자열
+
+    tags = models.CharField(max_length=100, blank=True)
+    langlat = models.CharField(max_length=50, blank=True,
+                               validators=[lnglat_validator],
+                               help_text='위도/경도 포맷으로 입력'
+                               )
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
