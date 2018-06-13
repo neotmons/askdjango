ef3e751e797e27be840ee2e4753d63ecf80e19f3
neotmons <neotmons@gmail.com>
2018-06-13 23:12:56 +0900

    각 앱마다 url namespace 적용

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import url
 from blog import views
 
 urlpatterns = [
-    url(r'^$', views.post_list),
-    url(r'^(?P<id>\d)/$', views.post_detail),
+    url(r'^$', views.post_list, name='post_list'),
+    url(r'^(?P<id>\d)/$', views.post_detail, name='post_detail'),
 
 ]
