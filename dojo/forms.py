22a3d84bab9b84e2ed999b649a24898f573f68e8
neotmons <neotmons@gmail.com>
2018-06-16 23:38:10 +0900

    javascript를 통해 dojo.models.post.user_agent 필드값 채움

--- dojo/forms.py
+++ dojo/forms.py
@@ -7,7 +7,11 @@ class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ['title', 'content']
+        fields = ['title', 'content', 'user_agent']
+
+        widgets = {
+            'user_agent': forms.HiddenInput,
+        }
     #title = forms.CharField(validators=[min_length_3_validator])
     #content = forms.CharField(widget=forms.Textarea)
     '''
